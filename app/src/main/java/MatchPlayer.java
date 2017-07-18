62e87bdb3780d0607f385d5280c5d655da65376a
bkmiecik12 <bkmiecik12@gmail.com>
2017-07-18 16:52:28 +0200

    Initial commit

---
+++ app/src/main/java/MatchPlayer.java
@@ -0,0 +1,28 @@
+import android.os.SystemClock;
+import android.widget.Chronometer;
+
+/**
+ * Created by bkmiecik on 17.07.17.
+ */
+public class MatchPlayer {
+    private Chronometer chronometer;
+    private long totalTime;
+
+    public MatchPlayer(){
+        chronometer.setBase(System.currentTimeMillis());
+    }
+
+    public void sub_in(){
+        chronometer.setBase(System.currentTimeMillis());
+        chronometer.start();
+    }
+
+    public void sub_out(){
+        chronometer.stop();
+        totalTime+= System.currentTimeMillis()-chronometer.getBase();
+    }
+
+    public long getTime(){
+        return totalTime;
+    }
+}
---
+++ app/src/main/java/Player.java
@@ -0,0 +1,30 @@
+import android.widget.Chronometer;
+
+/**
+ * Created by bkmiecik on 17.07.17.
+ */
+public class Player {
+    private String name;
+    private int number;
+
+    public Player(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
